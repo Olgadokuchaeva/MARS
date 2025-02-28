e972ed7c063a9e974a8914060b2613571a9caf7c
Ученик <olga1809dok@gmail.com>
2025-02-28 18:06:48 +0300

    1

--- 1.py
+++ 1.py
@@ -42,6 +42,37 @@ def return_sample_page():
                   </body>
                 </html>"""
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta charset="utf-8">
+                        <title>Варианты выбора</title>
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                      </head>
+                      <body>
+                        <h1>Моё предложение: {planet_name}!</h1>
+                        <div class="alert alert-secondary" role="alert">
+                            Человечество вырастает из детства.
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                            Человечеству мала одна планета.
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                            Мы сделаем обитаемыми безжизненные пока планеты.
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                            И начнем с Марса!
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                            Присоединяйся!
+                        </div>
+                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                      </body>
+                    </html>"""
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
