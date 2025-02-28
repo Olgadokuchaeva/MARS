b2cc6ba363a17de45d261290cb4e6db454f600f6
Ученик <olga1809dok@gmail.com>
2025-02-28 17:57:34 +0300

    1

--- 1.py
+++ 1.py
@@ -15,12 +15,29 @@ def return_sample_page():
                   <head>
                     <meta charset="utf-8">
                     <title>Привет, Марс!</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="../static/css/style.css')"/>
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
-                    <img src="{url_for('static', filename='img/img.png')}" 
-           alt="здесь должна была быть картинка, но не нашлась">
-                    <p>Вот она какая, красная планета.</p>
+                    <img src="{url_for('static', filename='img/img_1.png')}" 
+                    alt="здесь должна была быть картинка, но не нашлась">
+                    <div class="alert alert-secondary" role="alert">
+                        Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                        Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                        Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                        И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                        Присоединяйся!
+                    </div>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
                   </body>
                 </html>"""
 
