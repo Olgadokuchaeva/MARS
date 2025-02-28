1f5f37b26086df5314c0eb60530f3c53d97ae29a
Ученик <olga1809dok@gmail.com>
2025-02-28 17:59:18 +0300

    1

--- 1.py
+++ 1.py
@@ -14,9 +14,10 @@ def return_sample_page():
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
-                    <title>Привет, Марс!</title>
+                    <meta charset="utf-8">
+                    <title>Привет, марс!</title>
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-                    <link rel="stylesheet" type="text/css" href="../static/css/style.css')"/>
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
